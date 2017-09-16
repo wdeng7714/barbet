21ec0568fa4ec0e6983f7321dd7187328c77caa7
wendydeng <wendy9852@hotmail.com>
2017-09-16 18:43:38 -0400

    make details page ui

--- Wagify/app/src/main/java/layout/Details.java
+++ Wagify/app/src/main/java/layout/Details.java
@@ -7,8 +7,13 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import com.example.angelachang.wagify.R;
+
+import java.util.ArrayList;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -65,7 +70,8 @@ public class Details extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_details, container, false);
+        View v = inflater.inflate(R.layout.fragment_details, container, false);
+        return v;
     }
 
     // TODO: Rename method, update argument and hook method into UI event
